e74f0bd3245343f2da2b186972d86fa465eff94c
administrator <administrator@iispl>
2026-08-10 12:10:39 +0530

    added invalid XML structure exception and response generation exception

--- src/com/iispl/exception/InvalidXmlStructureException.java
+++ src/com/iispl/exception/InvalidXmlStructureException.java
@@ -1,5 +1,8 @@
 package com.iispl.exception;
 
 public class InvalidXmlStructureException extends Exception{
-
+	@Override
+	public String getMessage() {
+		return "XML structure is invalid";
+	}
 }
--- src/com/iispl/exception/ResponseGenerationException.java
+++ src/com/iispl/exception/ResponseGenerationException.java
@@ -2,4 +2,8 @@ package com.iispl.exception;
 
 public class ResponseGenerationException extends Exception{
 
+	@Override
+	public String getMessage() {
+		return "failed to generate response";
+	}
 }
